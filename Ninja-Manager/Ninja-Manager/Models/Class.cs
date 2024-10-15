5842abdfeae87ff9d02e5c0fe89bbc6f5d1b90aa
MatthijsCr <msj.cromwijk@student.avans.nl>
2024-10-15 12:50:38 +0200

    .

---
+++ Ninja-Manager/Ninja-Manager/Models/Class.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Ninja_Manager.Models
+{
+    public class NinjaDbContext : DbContext
+    {
+        public NinjaDbContext()
+        {
+        }
+
+        public NinjaDbContext(DbContextOptions<NinjaDbContext> options)
+            : base(options)
+        {
+        }
+
+        private IConfiguration Configuration => new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json")
+            .Build();
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            => optionsBuilder.UseSqlServer(Configuration.GetConnectionString("Ninja"));
+    }
+}
---
+++ Ninja-Manager/Ninja-Manager/Models/ContextFactory.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore;
+using Ninja_Manager.Models;
+
+namespace BumboApp.Models
+{
+    public class ContextFactory : IDesignTimeDbContextFactory<NinjaDbContext>
+    {
+        public ContextFactory()
+        {
+        }
+
+        private IConfiguration Configuration => new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory())
+          .AddJsonFile("appsettings.json")
+          .Build();
+
+        public NinjaDbContext CreateDbContext(string[] args)
+        {
+
+            var builder = new DbContextOptionsBuilder<NinjaDbContext>();
+            builder.UseSqlServer(Configuration.GetConnectionString("NinjaDb"));
+
+            return new NinjaDbContext(builder.Options);
+        }
+    }
+}
