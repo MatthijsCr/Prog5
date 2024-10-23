5ec189d787d215c74d57f694e57ecedc17c87167
MatthijsCr <msj.cromwijk@student.avans.nl>
2024-10-23 19:23:38 +0200

    de buy methode gemaakt waarin het nu nog mogelijk is om iets in te ruilen, maar niks terug te krijgen omdat je daarna niet genoeg geld hebt.

--- Ninja-Manager/Ninja-Manager/Controllers/ShopController.cs
+++ Ninja-Manager/Ninja-Manager/Controllers/ShopController.cs
@@ -6,19 +6,49 @@ namespace Ninja_Manager.Controllers
 {
     public class ShopController : MainController
     {
-        public IActionResult Index(int NinjaId)
+        public IActionResult Index(int NinjaId, Category? Type)
         {
-            List <Gear> totalGear = Context.Gears.ToList();
+            List<Gear> totalGear = Context.Gears.ToList();
+            ViewBag.Ninja = Context.Ninjas.Find(NinjaId).Name;
+            ViewBag.NinjaId = NinjaId;
+            ViewBag.NinjaGold = Context.Ninjas.Find(NinjaId).Gold;
+            ViewBag.Type = Type;
+            ViewBag.NinjaGear = Context.Ninjas.Find(NinjaId).GearForNinja;
             List<Gear> ninjaGear = Context.Ninjas.Find(NinjaId).GearForNinja;
             if (ninjaGear == null)
             {
-                RedirectToAction("Index","Ninja");
+                ninjaGear = new List<Gear>();
             }
             foreach (Gear g in totalGear)
             {
-                if(ninjaGear.Contains(g)) {totalGear.Remove(g);}
+                if (ninjaGear.Contains(g)) { totalGear.Remove(g); }
             }
+            ViewBag.NinjaGear = ninjaGear;
+            ViewBag.Categories = new List<Category> { Category.Ring, Category.Feet, Category.Chest, Category.Head, Category.Chest };
+            totalGear.OrderBy(t => t.Cost);
             return View(totalGear);
+        }
+
+        [HttpPost]
+        public IActionResult Buy(int gearId, int ninjaId)
+        {
+            Ninja ninja = Context.Ninjas.Find(ninjaId);
+            Gear gear = Context.Gears.Find(gearId);
+            if (!(ninja == null || gear == null))
+            {
+                if(ninja.GearForNinja.Any(g => g.Type == gear.Type))
+                {
+                    Gear geartoRemove = ninja.GearForNinja.Where(g => g.Type == gear.Type).First();
+                    ninja.Gold += geartoRemove.Cost;
+                    ninja.GearForNinja.Remove(geartoRemove);
+                }
+                if (ninja.Gold >= gear.Cost) 
+                {
+                    ninja.Gold -= gear.Cost;
+                    ninja.GearForNinja.Add(gear);
+                }
+            }
+            return RedirectToAction("Index");
         }
     }
 }
--- Ninja-Manager/Ninja-Manager/Models/NinjaManagerDbContext.cs
+++ Ninja-Manager/Ninja-Manager/Models/NinjaManagerDbContext.cs
@@ -30,36 +30,39 @@ namespace Ninja_Manager.Models
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Gear>().HasData(
+            new List<Gear>
+            {
                 // Head Gear
-                new Gear { Id = 1, Name = "Rusty Helm", intelligence = 1, strength = 1, agility = 0, Cost = 50, type = Category.Head },
-                new Gear { Id = 2, Name = "Iron Helm", intelligence = 2, strength = 3, agility = 1, Cost = 150, type = Category.Head },
-                new Gear { Id = 3, Name = "Crown of Wisdom", intelligence = 5, strength = 2, agility = 0, Cost = 300, type = Category.Head },
+                new Gear { Id = 1, Name = "Rusty Helm", Intelligence = 1, Strength = 1, Agility = 0, Cost = 50, Type = Category.Head },
+                new Gear { Id = 2, Name = "Iron Helm", Intelligence = 2, Strength = 3, Agility = 1, Cost = 150, Type = Category.Head },
+                new Gear { Id = 3, Name = "Crown of Wisdom", Intelligence = 5, Strength = 2, Agility = 0, Cost = 300, Type = Category.Head },
 
                 // Chest Gear
-                new Gear { Id = 4, Name = "Leather Armor", intelligence = 1, strength = 2, agility = 1, Cost = 60, type = Category.Chest },
-                new Gear { Id = 5, Name = "Iron Chestplate", intelligence = 2, strength = 4, agility = 1, Cost = 180, type = Category.Chest },
-                new Gear { Id = 6, Name = "Dragonscale Chest", intelligence = 3, strength = 6, agility = 2, Cost = 350, type = Category.Chest },
+                new Gear { Id = 4, Name = "Leather Armor", Intelligence = 1, Strength = 2, Agility = 1, Cost = 60, Type = Category.Chest },
+                new Gear { Id = 5, Name = "Iron Chestplate", Intelligence = 2, Strength = 4, Agility = 1, Cost = 180, Type = Category.Chest },
+                new Gear { Id = 6, Name = "Dragonscale Chest", Intelligence = 3, Strength = 6, Agility = 2, Cost = 350, Type = Category.Chest },
 
                 // Hand Gear
-                new Gear { Id = 7, Name = "Cloth Gloves", intelligence = 1, strength = 1, agility = 1, Cost = 40, type = Category.Hand },
-                new Gear { Id = 8, Name = "Leather Gloves", intelligence = 2, strength = 2, agility = 2, Cost = 100, type = Category.Hand },
-                new Gear { Id = 9, Name = "Gauntlets of Power", intelligence = 1, strength = 5, agility = 1, Cost = 250, type = Category.Hand },
+                new Gear { Id = 7, Name = "Cloth Gloves", Intelligence = 1, Strength = 1, Agility = 1, Cost = 40, Type = Category.Hand },
+                new Gear { Id = 8, Name = "Leather Gloves", Intelligence = 2, Strength = 2, Agility = 2, Cost = 100, Type = Category.Hand },
+                new Gear { Id = 9, Name = "Gauntlets of Power", Intelligence = 1, Strength = 5, Agility = 1, Cost = 250, Type = Category.Hand },
 
                 // Feet Gear
-                new Gear { Id = 10, Name = "Worn Boots", intelligence = 0, strength = 1, agility = 2, Cost = 50, type = Category.Feet },
-                new Gear { Id = 11, Name = "Iron Boots", intelligence = 1, strength = 3, agility = 2, Cost = 130, type = Category.Feet },
-                new Gear { Id = 12, Name = "Boots of Swiftness", intelligence = 0, strength = 2, agility = 6, Cost = 280, type = Category.Feet },
+                new Gear { Id = 10, Name = "Worn Boots", Intelligence = 0, Strength = 1, Agility = 2, Cost = 50, Type = Category.Feet },
+                new Gear { Id = 11, Name = "Iron Boots", Intelligence = 1, Strength = 3, Agility = 2, Cost = 130, Type = Category.Feet },
+                new Gear { Id = 12, Name = "Boots of Swiftness", Intelligence = 0, Strength = 2, Agility = 6, Cost = 280, Type = Category.Feet },
 
                 // Ring Gear
-                new Gear { Id = 13, Name = "Copper Ring", intelligence = 1, strength = 1, agility = 1, Cost = 30, type = Category.Ring },
-                new Gear { Id = 14, Name = "Silver Ring", intelligence = 2, strength = 1, agility = 2, Cost = 120, type = Category.Ring },
-                new Gear { Id = 15, Name = "Ring of the Ancients", intelligence = 4, strength = 0, agility = 4, Cost = 270, type = Category.Ring },
+                new Gear { Id = 13, Name = "Copper Ring", Intelligence = 1, Strength = 1, Agility = 1, Cost = 30, Type = Category.Ring },
+                new Gear { Id = 14, Name = "Silver Ring", Intelligence = 2, Strength = 1, Agility = 2, Cost = 120, Type = Category.Ring },
+                new Gear { Id = 15, Name = "Ring of the Ancients", Intelligence = 4, Strength = 0, Agility = 4, Cost = 270, Type = Category.Ring },
 
                 // Necklace Gear
-                new Gear { Id = 16, Name = "Wooden Necklace", intelligence = 1, strength = 0, agility = 1, Cost = 40, type = Category.Necklace },
-                new Gear { Id = 17, Name = "Silver Pendant", intelligence = 3, strength = 1, agility = 2, Cost = 140, type = Category.Necklace },
-                new Gear { Id = 18, Name = "Amulet of the Phoenix", intelligence = 5, strength = 0, agility = 3, Cost = 320, type = Category.Necklace }
-            );
+                new Gear { Id = 16, Name = "Wooden Necklace", Intelligence = 1, Strength = 0, Agility = 1, Cost = 40, Type = Category.Necklace },
+                new Gear { Id = 17, Name = "Silver Pendant", Intelligence = 3, Strength = 1, Agility = 2, Cost = 140, Type = Category.Necklace },
+                new Gear { Id = 18, Name = "Amulet of the Phoenix", Intelligence = 5, Strength = 0, Agility = 3, Cost = 320, Type = Category.Necklace }
+            });
+
         }
     }
 }
