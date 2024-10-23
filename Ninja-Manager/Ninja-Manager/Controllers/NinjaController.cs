a073aa5d804537a36616a330320f76eb06d37920
Kevin <k.janssen11@student.avans.nl>
2024-10-23 16:26:59 +0200

    Added ninja creating functionality

--- Ninja-Manager/Ninja-Manager/Controllers/NinjaController.cs
+++ Ninja-Manager/Ninja-Manager/Controllers/NinjaController.cs
@@ -1,13 +1,36 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using Ninja_Manager.Models;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using Ninja_Manager.Models;
 
 namespace Ninja_Manager.Controllers
 {
     public class NinjaController : MainController
     {
+        NinjaManagerDbContext context = new();
+
 
         public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(string name)
+        {
+            if (string.IsNullOrEmpty(name))
+            {
+                TempData["ErrorMessage"] = "Name cannot be empty. Please provide a valid name.";
+                return RedirectToAction("Index");
+            }
+
+            context.Ninjas.Add(new Ninja { Name = name });
+            context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Inventory()
         {
             return View();
         }
