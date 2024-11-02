f8a825494aff0be9b2514a9ac82160071c4f2837
MatthijsCr <msj.cromwijk@student.avans.nl>
2024-11-02 10:02:08 +0100

    extra changes voor consistency en bonus 2

--- Ninja-Manager/Ninja-Manager/Models/NinjaManagerDbContext.cs
+++ Ninja-Manager/Ninja-Manager/Models/NinjaManagerDbContext.cs
@@ -33,32 +33,32 @@ namespace Ninja_Manager.Models
                 // Head Gear
                 new Gear { Id = 1, Name = "Rusty Helm", Intelligence = 1, Strength = 1, Agility = 0, Cost = 50, Type = Category.Head },
                 new Gear { Id = 2, Name = "Iron Helm", Intelligence = 2, Strength = 3, Agility = 1, Cost = 150, Type = Category.Head },
-                new Gear { Id = 3, Name = "Crown of Wisdom", Intelligence = 5, Strength = 2, Agility = 0, Cost = 300, Type = Category.Head },
+                new Gear { Id = 3, Name = "Crown of Wisdom", Intelligence = 5, Strength = 3, Agility = 1, Cost = 210, Type = Category.Head },
 
                 // Chest Gear
-                new Gear { Id = 4, Name = "Leather Armor", Intelligence = 1, Strength = 2, Agility = 1, Cost = 60, Type = Category.Chest },
+                new Gear { Id = 4, Name = "Leather Armor", Intelligence = 1, Strength = 2, Agility = 1, Cost = 100, Type = Category.Chest },
                 new Gear { Id = 5, Name = "Iron Chestplate", Intelligence = 2, Strength = 4, Agility = 1, Cost = 180, Type = Category.Chest },
-                new Gear { Id = 6, Name = "Dragonscale Chest", Intelligence = 3, Strength = 6, Agility = 2, Cost = 350, Type = Category.Chest },
+                new Gear { Id = 6, Name = "Dragonscale Chest", Intelligence = 3, Strength = 6, Agility = 2, Cost = 280, Type = Category.Chest },
 
                 // Hand Gear
-                new Gear { Id = 7, Name = "Cloth Gloves", Intelligence = 1, Strength = 1, Agility = 1, Cost = 40, Type = Category.Hands },
-                new Gear { Id = 8, Name = "Leather Gloves", Intelligence = 2, Strength = 2, Agility = 2, Cost = 100, Type = Category.Hands },
-                new Gear { Id = 9, Name = "Gauntlets of Power", Intelligence = 1, Strength = 5, Agility = 1, Cost = 250, Type = Category.Hands },
+                new Gear { Id = 7, Name = "Cloth Gloves", Intelligence = 1, Strength = 1, Agility = 1, Cost = 70, Type = Category.Hands },
+                new Gear { Id = 8, Name = "Leather Gloves", Intelligence = 2, Strength = 2, Agility = 2, Cost = 140, Type = Category.Hands },
+                new Gear { Id = 9, Name = "Gauntlets of Power", Intelligence = 1, Strength = 5, Agility = 1, Cost = 190, Type = Category.Hands },
 
                 // Feet Gear
-                new Gear { Id = 10, Name = "Worn Boots", Intelligence = 0, Strength = 1, Agility = 2, Cost = 50, Type = Category.Feet },
-                new Gear { Id = 11, Name = "Iron Boots", Intelligence = 1, Strength = 3, Agility = 2, Cost = 130, Type = Category.Feet },
+                new Gear { Id = 10, Name = "Worn Boots", Intelligence = 0, Strength = 1, Agility = 2, Cost = 70, Type = Category.Feet },
+                new Gear { Id = 11, Name = "Iron Boots", Intelligence = 1, Strength = 3, Agility = 2, Cost = 150, Type = Category.Feet },
                 new Gear { Id = 12, Name = "Boots of Swiftness", Intelligence = 0, Strength = 2, Agility = 6, Cost = 280, Type = Category.Feet },
 
                 // Ring Gear
-                new Gear { Id = 13, Name = "Copper Ring", Intelligence = 1, Strength = 1, Agility = 1, Cost = 30, Type = Category.Ring },
-                new Gear { Id = 14, Name = "Silver Ring", Intelligence = 2, Strength = 1, Agility = 2, Cost = 120, Type = Category.Ring },
-                new Gear { Id = 15, Name = "Ring of the Ancients", Intelligence = 4, Strength = 0, Agility = 4, Cost = 270, Type = Category.Ring },
+                new Gear { Id = 13, Name = "Copper Ring", Intelligence = 1, Strength = 1, Agility = 1, Cost = 70, Type = Category.Ring },
+                new Gear { Id = 14, Name = "Silver Ring", Intelligence = 2, Strength = 1, Agility = 2, Cost = 110, Type = Category.Ring },
+                new Gear { Id = 15, Name = "Ring of the Ancients", Intelligence = 4, Strength = 0, Agility = 4, Cost = 160, Type = Category.Ring },
 
                 // Necklace Gear
                 new Gear { Id = 16, Name = "Wooden Necklace", Intelligence = 1, Strength = 0, Agility = 1, Cost = 40, Type = Category.Necklace },
-                new Gear { Id = 17, Name = "Silver Pendant", Intelligence = 3, Strength = 1, Agility = 2, Cost = 140, Type = Category.Necklace },
-                new Gear { Id = 18, Name = "Amulet of the Phoenix", Intelligence = 5, Strength = 0, Agility = 3, Cost = 320, Type = Category.Necklace }
+                new Gear { Id = 17, Name = "Silver Pendant", Intelligence = 3, Strength = 1, Agility = 2, Cost = 130, Type = Category.Necklace },
+                new Gear { Id = 18, Name = "Amulet of the Phoenix", Intelligence = 5, Strength = 3, Agility = 3, Cost = 250, Type = Category.Necklace }
             );
         }
     }
